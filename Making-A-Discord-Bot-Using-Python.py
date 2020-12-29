f456be85b2a7183a1ec719e9b5856fcc471d3089
EpicCodingBeastPro <76170598+EpicCodingBeastPro@users.noreply.github.com>
2020-12-29 20:36:52 +0530

    Add files via upload

---
+++ Making-A-Discord-Bot-Using-Python.py
@@ -0,0 +1,18 @@
+
+import discord
+from discord.ext import commands 
+
+TOKEN = "enter you token here"
+
+client = commands.Bot(command_prefix="@")#or anything you want buy which you will call this function 
+
+@client.event()
+async def on_ready():
+	print("Hey there I am ready")#this is just to know that the bot is online 
+
+@client.command()
+async def hello(ctx):
+	await ctx.send("Hey there how are you doing")#so you can do the same way anything you want 
+
+client.run(TOKEN)#so in the satrting we gave like TOKEN so we are running the bot using this or simply you can just insert your token directly to see how to see the token then see the above things
+	
